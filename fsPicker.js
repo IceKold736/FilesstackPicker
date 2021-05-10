8ddb0aa648694d0b8baaff50f9a8cb9e7e7d650e
IceKold736 <36084357+IceKold736@users.noreply.github.com>
2021-05-10 16:18:49 -0500

    Add files via upload

--- fsPicker.js
+++ fsPicker.js
@@ -1,23 +1,108 @@
 window.addEventListener('DOMContentLoaded', function () {
     const apikey = 'AtwRBFYzSCq5JHi4KX9ALz';
     const client = filestack.init(apikey);
-    let ul;
     const options = {
-        displayMode: 'inline',
+        displayMode: 'overlay',
         container: '#inline',
         maxFiles: 5,
         uploadInBackground: false,
-        onUploadDone: (res) => console.log(res),
+        onUploadDone: (res) => getURL(res),
     };
+
+    var thumbnail = "";
+    var fileType = "";
+
+    function getURL(response)
+    {
+    	let filesUploaded = '<h2>Files Uploaded:</h2>';
+    	for (var i = 0 ; i <= response.filesUploaded.length -1; i++)
+    	 {
+    	 	fileType = detectFile(response.filesUploaded[i]);
+
+    	 	if(fileType == "doc" || fileType == "image")
+    	 	{
+    	 		filesUploaded +=`
+    		
+    			<li> 
+    				<a target="_blank" href="${response.filesUploaded[i].url}">
+    					${response.filesUploaded[i].filename} - 
+    					<img src="${thumbnail}" style="width:150px; padding: 10px;"> 
+    				</a>
+    			</li>
+    		
+    		`;
+    	 	}
+    	 	else
+    	 	{
+    	 		filesUploaded +=`
+    		
+    			<li> 
+    				<a target="_blank" href="${response.filesUploaded[i].url}" download>
+    					${response.filesUploaded[i].filename}     					 
+    				</a>
+    			</li>
+
+    		`;
+    	 	}	
+    	}    		
+    	document.getElementById('filesList').innerHTML = filesUploaded;
+    }
+
+    function detectFile(item)
+    {
+    	var file = item;
+    	console.log(item.mimetype);
+    	if(item.mimetype.match( "application/\*"))
+    	{
+    		createThumbnail(file);
+    		console.log("doc");
+    		return "doc";
+    	}
+    	else if(item.mimetype.match("image/\*"))
+    	{
+    		thumbnail = item.url;
+    		console.log("img");
+    		return "image";
+
+    	}
+    	else
+    	{
+    		console.log("else");
+    		return "else";
+
+    	}
+
+    }
+
+    function createThumbnail(item)
+    {
+    	thumbnail = "https://cdn.filestackcontent.com/output=format:jpg/"+ item.handle;
+    }
 
     const picker = client.picker(options);
     const openBtn = document.getElementById('open');
-    const closeBtn = document.getElementById('close');
     openBtn.addEventListener('click', () => picker.open());
-    closeBtn.addEventListener('click', () => picker.close());
-
-
 });
 
 
 
+
+
+
+
+ //    function workflow(item)
+ //    {
+ //    	var xhr = new XMLHttpRequest();
+ //    	console.log(item.header);
+ //    	xhr.open('GET', 'https://cdn.filestackcontent.com/run_workflow=id:856f4619-320c-4583-9b47-ecfa62ee74bd/BDeUzLnTSaSspPh7zB1w', true);
+
+ //    	xhr.onreadystatechange = function ()
+ //    	{
+ //    		if(this.readyState == 4 && this.status == 200)
+ //    		{
+ //    			console.log(this.responseText);
+ //    		}
+ //    	}
+
+ //    	xhr.send();    	
+	// }
